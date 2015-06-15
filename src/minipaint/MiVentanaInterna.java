9f8943e391fadc9e5b7c30b838e12d588cb53e3b
PSanchezR <pablosanchez@correo.ugr.es>
2015-06-15 19:54:21 +0200

    Update MiVentanaInterna.java

--- src/minipaint/MiVentanaInterna.java
+++ src/minipaint/MiVentanaInterna.java
@@ -1,8 +1,14 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<<copyright 2013 Pablo Sánchez Robles>>
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+     any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
 /*
  * MiVentanaInterna.java
  *
